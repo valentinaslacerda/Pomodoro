8a10f9b57d9df9980878e59f8d1ca2a61a775dfb
nickolas-silva <nickolas.silva@alunos.ufersa.edu.br>
2023-07-19 15:07:07 -0300

    add colors

---
+++ pomodoro_app/lib/colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+const btnVermelho = Color(0xffA82B3A);
+const vermelho = Color(0xffEA5E57);
+const verde = Color(0xffE1ECC8);
+const btnVerde = Color(0xffA0C49D);
+const azul = Color(0xff1F6E8C);
+const btnAzul = Color(0xff0E2954);
+const texto = Color(0xffFFFFFF);
+const containerP = Color(0xffF28F80);
+const containerS = Color(0xffE1ECC8);
+const containerT = Color(0xff84A7A1);
+
