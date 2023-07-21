787f1dc851ac666995fe538161a4a71f96f588d3
Valentina Lacerda <valentinaslacerda07@gmail.com>
2023-07-21 11:13:00 -0300

    UI do app

--- pomodoro_app/lib/colors.dart
+++ pomodoro_app/lib/colors.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 
-const btnVermelho = Color(0xffA82B3A);
-const vermelho = Color(0xffEA5E57);
-const verde = Color(0xffE1ECC8);
-const btnVerde = Color(0xffA0C49D);
-const azul = Color(0xff1F6E8C);
-const btnAzul = Color(0xff0E2954);
-const texto = Color(0xffFFFFFF);
-const containerP = Color(0xffF28F80);
-const containerS = Color(0xffE1ECC8);
-const containerT = Color(0xff84A7A1);
-
+class MyColors {
+  static const btnVermelho = Color(0xffA82B3A);
+  static const vermelho = Color(0xffEA5E57);
+  static const verde = Color(0xffE1ECC8);
+  static const btnVerde = Color(0xffA0C49D);
+  static const azul = Color(0xff1F6E8C);
+  static const btnAzul = Color(0xff0E2954);
+  static const texto = Color(0xffFFFFFF);
+  static const containerP = Color(0xffF28F80);
+  static const containerS = Color(0xffE1ECC8);
+  static const containerT = Color(0xff84A7A1);
+}
